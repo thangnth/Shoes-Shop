80d16450b9820184a5240054d86f02f7e0d84840
thangnth <thangnth0511@gmail.com>
2023-08-24 10:55:58 +0700

    first commit

---
+++ ex-react/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ShoesShop from "./Ex_ShoesShop/ShoesShop";
+// import StateCar from "./Ex_CarStore/StateCar";
+
+function App() {
+  return (
+    <>
+      {/* <StateCar /> */}
+      <ShoesShop />
+    </>
+  );
+}
+
+export default App;
